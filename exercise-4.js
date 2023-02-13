4cd877df5478e498146e4b31e3361b8d2785954e
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-02-13 11:33:44 +0100

    fix errors

--- exercise-4.js
+++ exercise-4.js
@@ -1,28 +1,27 @@
 function nicknameMap(persons) {  
-  let nickname = [];
-  nickname = persons.map(function(person){
-    return person.name + '-' + person.age;
-  })
-  return nickname.forEach(nick => console.log(nick));  
+  let nickname =[]  
+  for (i of persons){        
+    nickname.push(i.name + "-" + i.age);
+  } 
+  return nickname
 }
 
 const persons = [
-  { name: 'Paul', age: 16 },
-  { name: 'George', age: 17 },
-  { name: 'Lucas', age: 21 },
-  { name: 'Marco', age: 32 },
-  { name: 'Peter', age: 18 },
-  { name: 'Carl', age: 13 },
-  { name: 'Simon', age: 24 },
-  { name: 'Mark', age: 15 },
-  { name: 'Sandra', age: 34 },
-  { name: 'Alice', age: 28 }
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
 ];
 
 let nicknames = nicknameMap(persons);
 console.log(persons);
-// console.log(nicknames); non sono riuscita a richiamare la variabile senza che mi desse undefined
-nicknameMap(persons)
+console.log(nicknames);
 
 
 
