4c6caeabde16b6aaaf8ea61d85f2aeecf80352cd
Monica Molini <122270090+MonicaMolini@users.noreply.github.com>
2023-02-13 11:32:32 +0100

    fix error

--- exercise.js
+++ exercise.js
@@ -1,9 +1,10 @@
-function inner(){      
-     return printName()    
-    function printName(){
-    let helloName = "Hello John";
-    console.log(helloName);
+function printName(){   
+ let helloName = "Hello John";
+   function inner(){    
+    console.log(helloName);;
+    }
+
+   return inner()
 }
 
-
-inner()
+printName()
